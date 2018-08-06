d63d5c6d3403f087171008b9968cb0010ce65c34
muthash <kathingo92@gmail.com>
2018-08-06 17:06:28 +0300

    [feature #155738450] Update Main Activity class

--- app/src/main/java/com/muthama/nbojavadevs/MainActivity.java
+++ app/src/main/java/com/muthama/nbojavadevs/MainActivity.java
@@ -2,12 +2,42 @@ package com.muthama.nbojavadevs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private List<ListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+        for(int i=0; i<=10; i++){
+            ListItem listItem = new ListItem(
+                    "@username ",
+                    "Date Joined:",
+                    "Number of Repositories:",
+                    "urlImage"
+            );
+
+            listItems.add(listItem);
+        }
+
+        adapter = new MyAdapter(listItems, this);
+        recyclerView.setAdapter(adapter);
     }
 }
