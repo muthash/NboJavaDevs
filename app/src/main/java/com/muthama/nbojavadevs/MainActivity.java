4305b586e5920bc917f78508659b3265057a3b2a
muthash <kathingo92@gmail.com>
2018-08-09 12:04:59 +0300

    [feature #155820645] Create layout file for DetailActivity

--- app/src/main/java/com/muthama/nbojavadevs/MainActivity.java
+++ app/src/main/java/com/muthama/nbojavadevs/MainActivity.java
@@ -10,34 +10,56 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-
-    private List<ListItem> listItems;
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImageDates = new ArrayList<>();
+    private ArrayList<String> mImageRepos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        listItems = new ArrayList<>();
-
-        for(int i=0; i<=10; i++){
-            ListItem listItem = new ListItem(
-                    "@username ",
-                    "Date Joined:",
-                    "Number of Repositories:",
-                    "urlImage"
-            );
-
-            listItems.add(listItem);
-        }
-
-        adapter = new MyAdapter(listItems, this);
-        recyclerView.setAdapter(adapter);
+        initImageBitmaps();
     }
+    private void initImageBitmaps(){
+
+        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mImageNames.add("Havasu Falls");
+
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mImageNames.add("Trondheim");
+
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mImageNames.add("Portugal");
+
+        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mImageNames.add("Rocky Mountain National Park");
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mImageNames.add("Mahahual");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mImageNames.add("Frozen Lake");
+
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+        mImageNames.add("White Sands Desert");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+        mImageNames.add("Austrailia");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mImageNames.add("Washington");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recyclerv_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mImageUrls, mImageNames, mImageNames, mImageNames);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
