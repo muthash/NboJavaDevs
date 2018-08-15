e767582cb2a9740791dc2647ebf240da4b7f7576
muthash <kathingo92@gmail.com>
2018-08-15 16:02:26 +0300

    [Feature #156008359] Implement share functionality

--- app/src/main/java/com/muthama/nbojavadevs/view/DetailActivity.java
+++ app/src/main/java/com/muthama/nbojavadevs/view/DetailActivity.java
@@ -1,7 +1,10 @@
 package com.muthama.nbojavadevs.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,12 +29,28 @@ public class DetailActivity extends AppCompatActivity {
 
             String imageUrl = user.getImageUrl();
             String imageName = user.getUsername();
+            String profile = user.getProfileUrl();
 
-            setImage(imageUrl, imageName);
+            setImage(imageUrl, imageName, profile);
+
+            Button shareButton = findViewById(R.id.share_button);
+            shareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_TEXT,
+                            "Check out this awesome developer @" + user.getUsername() + ", "
+                                    +
+                                    user.getProfileUrl());
+                    startActivity(Intent.createChooser(shareIntent, "Share link using"));
+                }
+            });
         }
     }
 
-    private void setImage(String imageUrl, String imageName){
+
+    private void setImage(String imageUrl, String imageName, String profile){
         ImageView image = findViewById(R.id.detail_image);
         Glide.with(this)
                 .asBitmap()
@@ -41,8 +60,8 @@ public class DetailActivity extends AppCompatActivity {
         TextView name = findViewById(R.id.detail_name);
         name.setText(imageName);
 
-        TextView smallName = findViewById(R.id.detail_small_name);
-        smallName.setText("@" + imageName);
+        TextView profileUrl = findViewById(R.id.detail_profile);
+        profileUrl.setText(profile);
 
     }
 }
